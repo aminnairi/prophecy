fdca3d1df6360079419b806cb1991560b8615e05
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-09-26 22:13:06 +0200

    updated the example to fetch users from jsonplaceholder and save them in a file while running in a timer an abort

--- example/node/index.ts
+++ example/node/index.ts
@@ -1,24 +1,39 @@
-import { kind } from "@prophecy/issue";
-import { getBufferFromFile, isFileAccessible } from "@prophecy/node";
+import { match } from "@prophecy/issue";
+import { writeToPath } from "@prophecy/node";
+import { sendRequestAtUrl, withAbortController } from "@prophecy/http";
+import { withDelay } from "@prophecy/time";
+import { toJson, toStringifiedJson, withUsers } from "./schemas/users";
 
-isFileAccessible("package.json").andThen(getBufferFromFile).and(String).on({
-  value: fileContent => {
-    console.log(`File content: ${fileContent}`);
-    return null;
-  },
-  issue: issue => {
-    switch (issue[kind]) {
-      case "GetBufferFromFileIssue":
-        console.error(`Error while opening file package.json (${issue.message})`);
+withAbortController()
+  .fork(abortController => {
+    return withDelay({ seconds: 3 }).on({
+      value: () => {
+        abortController.abort();
         return null;
-
-      case "IsFileAccessibleIssue":
-        console.error("File package.json is not accessible.");
-        return null;
-
-      case "UnexpectedIssue":
-        console.error(`Unexpected error: ${issue.error}`);
-        return null;
-    }
-  }
-});
+      },
+      issue: match({
+        UnexpectedIssue: () => console.error("Failed to abort the request after a delay.")
+      })
+    });
+  })
+  .fork(({ signal }) => {
+    return sendRequestAtUrl("https://jsonplaceholder.typicode.com/users", { signal })
+      .do(toJson)
+      .do(withUsers)
+      .do(toStringifiedJson({ pretty: true }))
+      .do(writeToPath("users.json"))
+      .on({
+        issue: match({
+          BadResponseIssue: issue => console.error(`Bad response from the server: ${issue.response.status}`),
+          JsonParseIssue: () => console.error("Failed to parse the response to json."),
+          RequestCanceledIssue: () => console.error("Request canceled, nothing to do."),
+          UserValidationIssue: () => console.error("Failed to validate the users from the response."),
+          UnexpectedIssue: issue => console.error(`Unexpected issue: ${issue.error}`),
+        })
+      });
+  })
+  .on({
+    issue: match({
+      UnexpectedIssue: () => console.error("Failed to instantiate an abort controller")
+    })
+  });

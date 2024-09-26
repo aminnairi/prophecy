241a9a7cd7f0ffae24df91160c11ae6a7fd479ca
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-09-26 22:12:02 +0200

    updated the example according to the recent changes

--- example/web/src/index.ts
+++ example/web/src/index.ts
@@ -1,48 +1,31 @@
 import { State } from "@prophecy/core";
-import { kind } from "@prophecy/issue";
-import { getElementById, onEvent, onText, setTextContent, EventKind } from "@prophecy/web/dom";
+import { match } from "@prophecy/issue";
+import { forId, forEvent, withInputText, setTextContent, EventKind } from "@prophecy/web/dom";
 import { whenEmpty } from "@prophecy/string";
 
 const numberOfKeystrokes = new State(0);
 
-getElementById("text")
-  .andThen(onEvent(EventKind.Input))
-  .andThen(onText)
-  .andThen(whenEmpty("John DOE"))
-  .andThen(text => getElementById("output").andThen(setTextContent(`Hello, ${text}`)))
+forId("text")
+  .do(forEvent(EventKind.Input))
+  .do(withInputText)
+  .do(whenEmpty("John DOE"))
+  .do(text => forId("output").do(setTextContent(`Hello, ${text}`)))
+  .fork(() => numberOfKeystrokes.next(value => value + 1))
   .on({
-    value: () => numberOfKeystrokes.next(value => value + 1),
-    issue: issue => {
-      switch (issue[kind]) {
-        case "ElementNotFoundIssue":
-          console.error(`Element with id ${issue.identifier} not found.`);
-          return null;
-
-        case "ElementNotInputIssue":
-          console.error("Element is not an input.");
-          return null;
-
-        case "UnexpectedIssue":
-          console.error(`Unexpected error: ${issue.error}`);
-          return null;
-      }
-    }
+    issue: match({
+      ElementNotFoundIssue: issue => console.error(`Element with id ${issue.identifier} not found.`),
+      ElementNotInputIssue: () => console.error("Element is not an input."),
+      UnexpectedIssue: issue => console.error(`Unexpected error: ${issue.error}`),
+    })
   });
 
 numberOfKeystrokes.on(value => {
-  return getElementById("keystrokes")
-    .andThen(setTextContent(`Number of keystrokes so far: ${value}`))
+  return forId("keystrokes")
+    .do(setTextContent(`Number of keystrokes so far: ${value}`))
     .on({
-      issue: issue => {
-        switch (issue[kind]) {
-          case "ElementNotFoundIssue":
-            console.error(`Element with id ${issue.identifier} is not found in the DOM`);
-            return null;
-
-          case "UnexpectedIssue":
-            console.error(`Unexpected error: ${issue.error}`);
-            return null;
-        }
-      }
+      issue: match({
+        ElementNotFoundIssue: issue => console.error(`Element with id ${issue.identifier} is not found in the DOM`),
+        UnexpectedIssue: issue => console.error(`Unexpected error: ${issue.error}`),
+      })
     });
 });

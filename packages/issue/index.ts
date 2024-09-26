fb0e90169f25eeb6920c9a27806d40a4419dbf48
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-09-26 22:10:54 +0200

    added a function to exhaustively handle issues

--- packages/issue/index.ts
+++ packages/issue/index.ts
@@ -9,3 +9,16 @@ export class UnexpectedIssue implements DiscriminatedIssue {
 
   public constructor(public readonly error: Error) {}
 }
+
+export function match<Issue extends DiscriminatedIssue>(patterns: { [Key in Issue[typeof kind]]: (issue: Extract<Issue, { [kind]: Key }>) => unknown }) {
+  return (issue: Issue): null => {
+    const issueKind = issue[kind];
+
+    if (issueKind in patterns) {
+      // @ts-ignore
+      return patterns[issueKind](issue);
+    } else {
+      throw new Error(`No handler for issue kind: ${issueKind}`);
+    }
+  }
+}

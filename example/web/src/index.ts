25790cf5a11921a052a2f5910e68971e2a3e55dd
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-09-26 12:46:11 +0200

    added a way to compute the number of strokes typed in the input field

--- example/web/src/index.ts
+++ example/web/src/index.ts
@@ -1,22 +1,48 @@
+import { State } from "@prophecy/core";
 import { kind } from "@prophecy/issue";
-import { getElementById, onEvent, onText, setTextContent } from "@prophecy/web";
+import { getElementById, onEvent, onText, setTextContent, EventKind } from "@prophecy/web/dom";
+import { whenEmpty } from "@prophecy/string";
 
-getElementById("text").andThen(onEvent("input")).andThen(onText).and(text => {
-  if (text.trim().length === 0) {
-    return "John DOE";
-  }
+const numberOfKeystrokes = new State(0);
 
-  return text;
-}).andThen(text => {
-  return getElementById("output").andThen(setTextContent(`Hello, ${text}`));
-}).onIssue(issue => {
-  switch (issue[kind]) {
-    case "ElementNotFoundIssue":
-      console.error("Element not found.");
-      return null;
+getElementById("text")
+  .andThen(onEvent(EventKind.Input))
+  .andThen(onText)
+  .andThen(whenEmpty("John DOE"))
+  .andThen(text => getElementById("output").andThen(setTextContent(`Hello, ${text}`)))
+  .on({
+    value: () => numberOfKeystrokes.next(value => value + 1),
+    issue: issue => {
+      switch (issue[kind]) {
+        case "ElementNotFoundIssue":
+          console.error(`Element with id ${issue.identifier} not found.`);
+          return null;
 
-    case "ElementNotInputIssue":
-      console.error("Element is not an input.");
-      return null;
-  }
+        case "ElementNotInputIssue":
+          console.error("Element is not an input.");
+          return null;
+
+        case "UnexpectedIssue":
+          console.error(`Unexpected error: ${issue.error}`);
+          return null;
+      }
+    }
+  });
+
+numberOfKeystrokes.on(value => {
+  return getElementById("keystrokes")
+    .andThen(setTextContent(`Number of keystrokes so far: ${value}`))
+    .on({
+      issue: issue => {
+        switch (issue[kind]) {
+          case "ElementNotFoundIssue":
+            console.error(`Element with id ${issue.identifier} is not found in the DOM`);
+            return null;
+
+          case "UnexpectedIssue":
+            console.error(`Unexpected error: ${issue.error}`);
+            return null;
+        }
+      }
+    });
 });

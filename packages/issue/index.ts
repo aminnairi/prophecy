4de34159b33835865a9d2f6addf94e2c96fdb1f7
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-09-26 12:48:09 +0200

    added a generic error for uncaught errors

--- packages/issue/index.ts
+++ packages/issue/index.ts
@@ -3,3 +3,9 @@ export const kind = Symbol("DiscriminatedIssueKind");
 export interface DiscriminatedIssue {
   [kind]: string
 }
+
+export class UnexpectedIssue implements DiscriminatedIssue {
+  public readonly [kind] = "UnexpectedIssue";
+
+  public constructor(public readonly error: Error) {}
+}

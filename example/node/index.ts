0c42c2d95170210b356031fdce860f673be58338
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-09-28 17:51:59 +0200

    applied the stylistic changes in method names

--- example/node/index.ts
+++ example/node/index.ts
@@ -1,32 +1,25 @@
 import { match } from "@prophecy/issue";
-import { writeToPath } from "@prophecy/node";
-import { sendRequestAtUrl, withAbortController } from "@prophecy/http";
-import { withDelay } from "@prophecy/time";
-import { toJson, toStringifiedJson, withUsers } from "./schemas/users";
+import { writeToPath } from "@prophecy/node/filesystem";
+import { abortAt, sendRequestAtUrl, withAbortController } from "@prophecy/http";
+import { toJson, toStringifiedJson, toUsers } from "./schemas/users";
+import { Future } from "@prophecy/core";
 
 withAbortController()
-  .fork(abortController => {
-    return withDelay({ seconds: 3 }).on({
-      value: () => {
-        abortController.abort();
-        return null;
-      },
-      issue: match({
-        UnexpectedIssue: () => console.error("Failed to abort the request after a delay.")
-      })
-    });
-  })
-  .do(({ signal }) => sendRequestAtUrl("https://jsonplaceholder.typicode.com/users", { signal }))
-  .do(toJson)
-  .do(withUsers)
-  .do(toStringifiedJson({ pretty: true }))
-  .do(writeToPath("users.json"))
-  .on({
-    issue: match({
+  .and(abortAt({ seconds: 5 }))
+  .and(({ signal }) => sendRequestAtUrl("https://jsonplaceholder.typicode.com/users", { signal }))
+  .and(toJson)
+  .and(toUsers)
+  .and(toStringifiedJson({ pretty: true }))
+  .and(writeToPath("users.json"))
+  .recover("BadResponseIssue", issue => new Future<number, never>((onValue) => onValue(123)))
+  .run({
+    onIssue: match({
       UnexpectedIssue: () => console.error("Failed to instantiate an abort controller"),
-      BadResponseIssue: issue => console.error(`Bad response from the server: ${issue.response.status}`),
       JsonParseIssue: () => console.error("Failed to parse the response to json."),
       RequestCanceledIssue: () => console.error("Request canceled, nothing to do."),
       UserValidationIssue: () => console.error("Failed to validate the users from the response."),
-    })
+    }),
+    onValue: value => {
+      return null;
+    }
   });
--- example/web/src/index.ts
+++ example/web/src/index.ts
@@ -1,31 +1,27 @@
 import { State } from "@prophecy/core";
 import { match } from "@prophecy/issue";
-import { forId, forEvent, withInputText, setTextContent, EventKind } from "@prophecy/web/dom";
+import { forId, forEvent, getInputValue, setTextContent, EventKind } from "@prophecy/web/dom";
 import { whenEmpty } from "@prophecy/string";
 
 const numberOfKeystrokes = new State(0);
 
 forId("text")
-  .do(forEvent(EventKind.Input))
-  .do(withInputText)
-  .do(whenEmpty("John DOE"))
-  .do(text => forId("output").do(setTextContent(`Hello, ${text}`)))
+  .and(forEvent(EventKind.Input))
+  .and(getInputValue)
+  .and(whenEmpty("John DOE"))
+  .and(text => forId("output").and(setTextContent(`Hello, ${text}`)))
   .fork(() => numberOfKeystrokes.next(value => value + 1))
-  .on({
-    issue: match({
-      ElementNotFoundIssue: issue => console.error(`Element with id ${issue.identifier} not found.`),
-      ElementNotInputIssue: () => console.error("Element is not an input."),
-      UnexpectedIssue: issue => console.error(`Unexpected error: ${issue.error}`),
-    })
-  });
+  .run(match({
+    ElementNotFoundIssue: issue => console.error(`Element with id ${issue.identifier} not found.`),
+    ElementNotInputIssue: () => console.error("Element is not an input."),
+    UnexpectedIssue: issue => console.error(`Unexpected error: ${issue.error}`),
+  }));
 
 numberOfKeystrokes.on(value => {
   return forId("keystrokes")
-    .do(setTextContent(`Number of keystrokes so far: ${value}`))
-    .on({
-      issue: match({
-        ElementNotFoundIssue: issue => console.error(`Element with id ${issue.identifier} is not found in the DOM`),
-        UnexpectedIssue: issue => console.error(`Unexpected error: ${issue.error}`),
-      })
-    });
+    .and(setTextContent(`Number of keystrokes so far: ${value}`))
+    .run(match({
+      ElementNotFoundIssue: issue => console.error(`Element with id ${issue.identifier} is not found in the DOM`),
+      UnexpectedIssue: issue => console.error(`Unexpected error: ${issue.error}`),
+    }));
 });

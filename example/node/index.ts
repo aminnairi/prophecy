f9766942902324d40c8d02148fc43e77710afdc7
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-09-28 21:13:58 +0200

    updated the example

--- example/node/index.ts
+++ example/node/index.ts
@@ -1,25 +1,25 @@
 import { match } from "@prophecy/issue";
 import { writeToPath } from "@prophecy/node/filesystem";
 import { abortAt, sendRequestAtUrl, withAbortController } from "@prophecy/http";
-import { toJson, toStringifiedJson, toUsers } from "./schemas/users";
+import { Users, toJson, toStringifiedJson, toUsers } from "./schemas/users";
 import { Future } from "@prophecy/core";
 
 withAbortController()
   .and(abortAt({ seconds: 5 }))
-  .and(({ signal }) => sendRequestAtUrl("https://jsonplaceholder.typicode.com/users", { signal }))
+  .and(({ abortController: { signal }, stopTimeout }) => {
+    return sendRequestAtUrl("https://jsonplaceholder.typicode.com/users", { signal })
+      .and(stopTimeout);
+  })
   .and(toJson)
   .and(toUsers)
+  .recover("UserValidationIssue", () => new Future<Users, never>(onValue => onValue([])))
   .and(toStringifiedJson({ pretty: true }))
   .and(writeToPath("users.json"))
-  .recover("BadResponseIssue", issue => new Future<number, never>((onValue) => onValue(123)))
   .run({
     onIssue: match({
       UnexpectedIssue: () => console.error("Failed to instantiate an abort controller"),
-      JsonParseIssue: () => console.error("Failed to parse the response to json."),
+      BadResponseIssue: () => console.error("Bad response from the server."),
       RequestCanceledIssue: () => console.error("Request canceled, nothing to do."),
-      UserValidationIssue: () => console.error("Failed to validate the users from the response."),
-    }),
-    onValue: value => {
-      return null;
-    }
+      JsonParseIssue: () => console.error("Failed to parse the users from the response."),
+    })
   });

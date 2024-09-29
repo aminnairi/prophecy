0fce8a46c3aebe4bf50dc9dd82691d58ef6c835c
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-09-29 18:12:14 +0200

    initial commit

---
+++ packages/node/readline.ts
@@ -0,0 +1,25 @@
+import { Future } from "@prophecy/core";
+import { DiscriminatedIssue, UnexpectedIssue, kind } from "@prophecy/issue";
+import { createInterface } from "readline/promises";
+import { ReadLineOptions } from "readline";
+
+export class ReadlineIssue implements DiscriminatedIssue {
+  public readonly [kind] = "ReadlineIssue";
+  public constructor(public readonly error: Error) {}
+}
+
+export const question = ({ question, ...options }: ReadLineOptions & { question: string }): Future<string, ReadlineIssue | UnexpectedIssue> => {
+  return new Future((onValue, onIssue) => {
+    const readlineInterface = createInterface(options);
+
+    readlineInterface.question(question).then(answer => {
+      readlineInterface.close();
+      onValue(answer);
+    }).catch(error => {
+      const normalizedError = error instanceof Error ? new ReadlineIssue(error) : new ReadlineIssue(new Error(String(error)));
+      onIssue(normalizedError);
+    });
+
+    return null;
+  });
+};

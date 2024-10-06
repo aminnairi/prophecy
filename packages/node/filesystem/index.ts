a702bb1791ab1150a3f8b3f3dd3d4a3fd95aec72
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-10-06 11:45:34 +0200

    multiple rework on names for functions and enumerations

--- packages/node/filesystem/index.ts
+++ packages/node/filesystem/index.ts
@@ -1,54 +1,54 @@
 import { Future, DiscriminatedIssue, UnexpectedIssue, kind } from "@prophecy/future";
 import { readFile, access, constants, writeFile } from "fs/promises";
 
-export enum FileAccess {
+export enum PathAccess {
   Readable,
   Writable,
   Visible,
   Executable
 }
 
-export class GetBufferFromFileIssue implements DiscriminatedIssue {
+export class ReadFileIssue implements DiscriminatedIssue {
   public readonly [kind] = "GetBufferFromFileIssue";
 
   public constructor(public readonly message: string) {}
 }
 
-export class IsFileAccessibleIssue implements DiscriminatedIssue {
+export class PathAccessIssue implements DiscriminatedIssue {
   public readonly [kind] = "IsFileAccessibleIssue";
 
   public constructor(public readonly message: string) {}
 }
 
-export const withBufferForFile = (path: string): Future<Buffer, GetBufferFromFileIssue | UnexpectedIssue> => {
+export const withBufferForFile = (path: string): Future<Buffer, ReadFileIssue | UnexpectedIssue> => {
   return Future.from((onValue, onIssue) => {
     readFile(path).then(buffer => {
       onValue(buffer);
     }).catch(error => {
       const message = error instanceof Error ? error.message : String(error);
-      onIssue(new GetBufferFromFileIssue(message));
+      onIssue(new ReadFileIssue(message));
     });
 
     return null;
   });
 };
 
-export const forFileWithAccess = (modes: FileAccess | Array<FileAccess>, path: string): Future<string, IsFileAccessibleIssue | UnexpectedIssue> => {
+export const pathAccessibleForMode = (modes: PathAccess | Array<PathAccess>, path: string): Future<string, PathAccessIssue | UnexpectedIssue> => {
   return Future.from((onValue, onIssue) => {
     const modesNormalized = Array.isArray(modes) ? modes : [modes];
 
     const modesCombined = modesNormalized.reduce((previousMode, currentMode) => {
       switch (currentMode) {
-        case FileAccess.Executable:
+        case PathAccess.Executable:
           return previousMode | constants.X_OK
 
-        case FileAccess.Readable:
+        case PathAccess.Readable:
           return previousMode | constants.R_OK;
 
-        case FileAccess.Writable:
+        case PathAccess.Writable:
           return previousMode | constants.W_OK
 
-        case FileAccess.Visible:
+        case PathAccess.Visible:
           return previousMode;
       }
     }, constants.F_OK);
@@ -57,14 +57,14 @@ export const forFileWithAccess = (modes: FileAccess | Array<FileAccess>, path: s
       onValue(path);
     }).catch(error => {
       const message = error instanceof Error ? error.message : String(error);
-      onIssue(new IsFileAccessibleIssue(message));
+      onIssue(new PathAccessIssue(message));
     });
 
     return null;
   });
 };
 
-export const writeStringToPath = (path: string, data: string): Future<void, UnexpectedIssue | UnexpectedIssue> => {
+export const writeStringToFile = (path: string, data: string): Future<void> => {
   return Future.from((onValue, onIssue) => {
     writeFile(path, data).then(() => {
       onValue();
@@ -78,10 +78,10 @@ export const writeStringToPath = (path: string, data: string): Future<void, Unex
 };
 
 export const writeToFile = ({ path }: { path: string }) => {
-  return (data: string): Future<void, UnexpectedIssue | UnexpectedIssue> => {
+  return (data: string): Future<string> => {
     return Future.from((onValue, onIssue) => {
       writeFile(path, data).then(() => {
-        onValue();
+        onValue(data);
       }).catch(error => {
         const errorNormalized = error instanceof Error ? error : new Error(String(error));
         onIssue(new UnexpectedIssue(errorNormalized));

bf4ce627c0f18f9cb092590b1ba4db83a1abbec0
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-11-23 22:01:58 +0100

    added an example of an http server

--- example/node/index.ts
+++ example/node/index.ts
@@ -2,6 +2,7 @@ import { Future, match } from "@prophecy/future";
 import { writeToFile } from "@prophecy/node/filesystem";
 import { abortAt, createAbortController, sendAbortableRequest } from "@prophecy/http";
 import { Users, toJson, toStringifiedJson, toUsers } from "./schemas/users";
+import { forAnyMethod, forAnyPath, forMethod, forPath, HttpMethod, listen, withRoute, withServer } from "@prophecy/node/http/http";
 
 createAbortController()
   .and(abortAt({ seconds: 5 }))
@@ -17,3 +18,54 @@ createAbortController()
     RequestCanceledIssue: () => console.error("Request canceled, nothing to do."),
     JsonParseIssue: () => console.error("Failed to parse the users from the response."),
   }));
+
+withServer()
+  .and(withRoute({
+    method: forMethod(HttpMethod.Get),
+    path: forPath("/hello"),
+    handler: () => Future.of((onValue) => onValue({
+      body: "world!",
+      statusCode: 200,
+      headers: {}
+    }))
+  }))
+  .and(withRoute({
+    path: forAnyPath,
+    method: forAnyMethod,
+    handler: () => Future.of(onValue => onValue({
+      body: "Not found",
+      statusCode: 404,
+      headers: {}
+    }))
+  }))
+  .and(listen({
+    port: 8000,
+    host: "0.0.0.0"
+  }))
+  .parallel(() => {
+    console.log("Starting HTTP server");
+    return null;
+  })
+  .run(match({
+    HostFormatInvalidIssue: () => {
+      console.error("Host format is invalid.");
+    },
+    HostOctetsInvalidIssue: () => {
+      console.error("Host octets invalid.");
+    },
+    HostOctetsRangeIssue: () => {
+      console.error("Hosts octets not in range.");
+    },
+    PortInfiniteIssue: () => {
+      console.error("Port is inifinite");
+    },
+    PortNegativeIssue: () => {
+      console.error("Port is negative");
+    },
+    PortNotNumberIssue: () => {
+      console.error("Port is not in range.");
+    },
+    UnexpectedIssue: () => {
+      console.error("An error occurred");
+    }
+  }))
